92bbdd7c9ce1054654bbec0ccd6ad570058e8c83
irenekyc <gugu_irene@hotmail.com>
2020-05-19 07:20:54 +0800

    mobile version and form validation set up

---
+++ index.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('email-form')
+const submitBtn = document.getElementById('submit-btn')
+const errorBtn = document.getElementById('error-b')
+const emailField = document.getElementById('email-field')
+
+submitBtn.addEventListener('click', (e)=>{
+    e.preventDefault()
+    if(emailField.validity.patternMismatch){
+        form.classList.add('error-p')
+        errorBtn.style.display="block"
+        emailField.addEventListener('input', e=>{
+            if(!emailField.validity.patternMismatch){
+                form.classList.remove('error-p')
+                errorBtn.style.display="none"
+
+            }
+        })
+    } else{
+        form.classList.add('success')
+        emailField.value=""
+    } 
+})
+
